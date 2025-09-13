2a0ef6da8a2aebfebeb2bf9590331171f75ebb38
Peter van Dam <peter@petervdam.nl>
2025-09-13 23:40:22 +0200

    Add phpstan

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,5 +5,13 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    if (!is_string($context['APP_ENV'])) {
+        throw new RuntimeException('APP_ENV must be a string');
+    }
+
+    if (!is_bool($context['APP_DEBUG'])) {
+        throw new RuntimeException('APP_DEBUG must be a boolean');
+    }
+
+    return new Kernel($context['APP_ENV'], $context['APP_DEBUG']);
 };
