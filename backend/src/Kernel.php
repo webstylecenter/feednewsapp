c38fac66b06a566c8a89807b0a925747cd56c156
Peter van Dam <peter@petervdam.nl>
2025-09-14 00:01:25 +0200

    Replaced abstract to final class in kernel

--- backend/src/Kernel.php
+++ backend/src/Kernel.php
@@ -5,7 +5,7 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-abstract class Kernel extends BaseKernel
+final Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
