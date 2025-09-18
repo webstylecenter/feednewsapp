cdbfbdf267bcfdc9d6955c7f99f5aa348c35d4fb
Peter van Dam <peter@petervdam.nl>
2025-09-18 19:55:39 +0200

    Update backend/src/User/Dto/Request/UserRequest.php
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- backend/src/User/Dto/Request/UserRequest.php
+++ backend/src/User/Dto/Request/UserRequest.php
@@ -11,19 +11,25 @@ use Symfony\Component\Validator\Constraints as Assert;
 final readonly class UserRequest
 {
     #[Assert\NotBlank]
+    #[Assert\Length(min: 1, max: 180)]
     #[Groups(['user:write'])]
     public string $name;
 
     #[Assert\NotBlank]
     #[Assert\Email]
+    #[Assert\Length(max: 254)]
     #[Groups(['user:write'])]
     public string $email;
 
     #[Assert\NotBlank]
+    #[Assert\Length(min: 8, max: 4096)]
+    #[Assert\NotCompromisedPassword]
     #[Groups(['user:write'])]
     public string $password;
 
     #[Groups(['user:write'])]
+    #[Assert\Url(protocols: ['http','https'])]
+    #[Assert\Length(max: 2048)]
     public ?string $avatar;
 
     public function __construct(
