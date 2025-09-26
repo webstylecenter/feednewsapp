5f0ac19b2dd9f0512da4b65ae235a2618edc80e5
Peter van Dam <peter@petervdam.nl>
2025-09-27 00:35:55 +0200

    Add svelte installation to frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()],
+    server: {
+        allowedHosts: ['feednews.ddev.site'],
+        host: true,
+        port: 5173,
+        strictPort: true
+    }
+});
